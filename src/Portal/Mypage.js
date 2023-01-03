b1b2a8e083d3b3e0688add5510ad531a69a258cd
elzex <g21220230f@edu.teu.ac.jp>
2023-01-04 03:12:11 +0900

    アブストラクトの投稿は完了（要リファクタリング）

--- src/Portal/Mypage.js
+++ src/Portal/Mypage.js
@@ -1,21 +1,28 @@
-import "../css/Form.css"
+import "../css/Form.css";
 import "../css/Mypage.css";
 import "../css/Tab.css";
 import "../css/Table.css";
+import "../css/popup.css";
+
+import { doc, setDoc, serverTimestamp, updateDoc, getDoc } from "firebase/firestore";
 
 import { userCheck } from "../firebaseModule/Authentication";
 import { setUserData } from "./Mypage/setProfileData";
-import { getUserData } from "../firebaseModule/Firestore";
+import { getUserData, db } from "../firebaseModule/Firestore";
 import { checkAbstract } from "./Mypage/abstract";
 
+let uid;
+let data;
 window.onload = function () {
     userCheck().then(async (user) => {
         //console.log(user.uid);
-        const data = await getUserData(user.uid);
+        uid = user.uid
+        data = await getUserData(uid);
         setUserData(data);
         checkAbstract(data);
-    }).catch(() => {
-        window.location.href = "../index.html";
+    }).catch((e) => {
+        console.log(e);
+        //window.location.href = "../index.html";
     });
 }
 
@@ -23,3 +30,26 @@ const back = document.getElementById("back");
 back.addEventListener("click", () => {
     window.location.href = "./Home.html";
 })
+
+const form = document.forms[0];
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const abs = [data.Title, e.target.absInput.value];
+    const abslist = [data.Name, data.Title, e.target.absInput.value]
+
+    const docRef = doc(db, "Account", uid);
+    const absRef = doc(db, "Abstract", "list");
+    updateDoc(docRef, {
+        abs: abs
+    }).catch((e) => {
+        console.log(e);
+    }).then(async () => {
+        updateDoc(absRef, {
+            [uid]: abslist
+        }).then(() => {
+            alert("投稿完了");
+            window.location.reload();
+        });
+    });
+});
--- src/Portal/Mypage/abstract.js
+++ src/Portal/Mypage/abstract.js
@@ -1,11 +1,11 @@
 const msg = document.getElementById("absDefault");
-const post = document.getElementById("postButton");
+const postbtn = document.getElementById("postButton");
 const body = document.getElementById("absBody");
 export function checkAbstract(data) {
 	msg.style.display = "none";
 	if (data.abs == undefined) {
 		//console.log("absナシ");
-		post.style.display = "block";
+		postbtn.style.display = "block";
 	} else {
 		//console.log("absアリ");
 		body.style.display = "block";
@@ -19,3 +19,15 @@ function setAbs(arr) {
 	title.textContent = arr[0];
 	absMain.textContent = arr[1];
 }
+
+const post = document.getElementById("post");
+const test = document.getElementById("test");
+post.addEventListener("click", () => {
+	//bg.style.display = "block";
+	test.style.display = "block";
+});
+
+const close = document.getElementById("close-popup");
+close.addEventListener("click", () => {
+	test.style.display = "none";
+});
