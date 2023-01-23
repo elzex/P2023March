1dc96e8fe9a978f59ea14dafcbfb99149f5ff716
Ryosuke Hibi <c0118232c1@edu.teu.ac.jp>
2023-01-23 10:52:11 +0900

    toCSV

--- src/toCSV/toCSV.js
+++ src/toCSV/toCSV.js
@@ -1,11 +1,11 @@
 import { collection, getDocs } from "firebase/firestore";
-import {}
+import { db } from "../firebaseModule/Firestore"
 
 import { userCheck } from "../firebaseModule/Authentication";
 
 import { collection, getDocs } from "firebase/firestore";
 
-const querySnapshot = await getDocs(collection(db, "cities"));
+const querySnapshot = await getDocs(collection(db, "Account"));
 querySnapshot.forEach((doc) => {
 	// doc.data() is never undefined for query doc snapshots
 	console.log(doc.id, " => ", doc.data());
