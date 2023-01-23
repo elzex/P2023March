e313be9116fad9515879232a5174c6822fc9ccf6
Ryosuke Hibi <c0118232c1@edu.teu.ac.jp>
2023-01-23 10:50:33 +0900

    master

---
+++ src/toCSV/toCSV.js
@@ -0,0 +1,12 @@
+import { collection, getDocs } from "firebase/firestore";
+import {}
+
+import { userCheck } from "../firebaseModule/Authentication";
+
+import { collection, getDocs } from "firebase/firestore";
+
+const querySnapshot = await getDocs(collection(db, "cities"));
+querySnapshot.forEach((doc) => {
+	// doc.data() is never undefined for query doc snapshots
+	console.log(doc.id, " => ", doc.data());
+});
