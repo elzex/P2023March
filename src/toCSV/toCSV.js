e140fc3cf0117c7085d8fb27032ab6c6196b23c1
Ryosuke Hibi <c0118232c1@edu.teu.ac.jp>
2023-01-23 13:59:04 +0900

    toCSV プログラム部分完成

--- src/toCSV/toCSV.js
+++ src/toCSV/toCSV.js
@@ -1,13 +1,13 @@
-import { db } from "../firebaseModule/Firestore"
-import { userCheck } from "../firebaseModule/Authentication";
+import { db } from "../firebaseModule/Firestore";
 
-import { collection, getDocs } from "firebase/firestore"
+import { collection, getDocs } from "firebase/firestore";
+import { } from "../firebaseModule/Authentication"
 
 
 window.onload = async function () {
 	const datas = await getAllData();
-	obj2arr(datas);
-	let tempData = [];
+	let arr = obj2arr(datas);
+	exportCSV(arr)
 }
 
 async function getAllData() {
@@ -24,14 +24,43 @@ async function getAllData() {
 }
 
 function obj2arr(obj) {
-	const key = ["Nmae", "Affiliation", "Job", "eMail", "PhoneNumber", "Presen", "Joint", "PreferredTime", "Schedule", "Support", "Comment", "signUpTime", "UserID"];
+	const key = ["Name", "Affiliation", "Job", "eMail", "PhoneNumber", "Presen", "Joint", "PreferredTime", "Schedule", "Support", "Comment", "signUpTime", "UserID"];
 	console.log(key);
 	console.log(obj);
+	console.log(obj[0][key[0]]);
 	let data = []
 	for (let i = 0; i < obj.length; i++) {
-		if (obj[i].Name != undefined) {
-			data[i] = [obj[i].Name, Object.values(obj[i])];
+		let tempArr = []
+		//console.log(obj[i][key[j]]);
+		for (let j = 0; j < key.length; j++) {
+			if (obj[i][key[j]] != undefined) {
+				if (j == 11) {
+					obj[i][key[j]] = obj[i][key[j]].toDate()
+				}
+				tempArr[j] = obj[i][key[j]];
+			} else {
+				tempArr[j] = "";
+			}
+		}
+		if (tempArr[i] != "") {
+			data.push(tempArr);
 		}
 	}
+	data.unshift(key);
 	console.log(data);
+	return data
 }
+
+function exportCSV(records) {
+	let data = records.map((record) => record.join(',')).join('\r\n');
+
+	let bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
+	let blob = new Blob([bom, data], { type: 'text/csv' });
+	let url = (window.URL || window.webkitURL).createObjectURL(blob);
+	let link = document.createElement('a');
+	link.download = 'result.csv';
+	link.href = url;
+	document.body.appendChild(link);
+	link.click();
+	document.body.removeChild(link);
+};
